c5795a8eb3c57793abbcd78fd69d3a4befd5d1d5
thefreshduke <scottyshaw1@gmail.com>
2016-07-22 00:02:14 -0700

    Created IBOutlets and IBActions

--- SpectraWorkshop-iOS1/ViewController.swift
+++ SpectraWorkshop-iOS1/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBAction func startTime(sender: AnyObject) {
+        startButton.enabled = false
+        startButton.hidden = true
+        stopButton.enabled = true
+        stopButton.hidden = false
+        timeLabel.text = "STARTED"
+    }
+    
+    @IBAction func stopTime(sender: AnyObject) {
+        startButton.enabled = true
+        startButton.hidden = false
+        stopButton.enabled = false
+        stopButton.hidden = true
+        timeLabel.text = "STOPPED"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = "00:00:00"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
