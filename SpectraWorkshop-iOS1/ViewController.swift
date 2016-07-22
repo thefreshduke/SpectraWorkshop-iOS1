484de3493b325a9684205a63c03b225624e0d3c5
thefreshduke <scottyshaw1@gmail.com>
2016-07-21 23:31:53 -0700

    Second commit

---
+++ SpectraWorkshop-iOS1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpectraWorkshop-iOS1
+//
+//  Created by Scotty Shaw on 7/21/16.
+//  Copyright © 2016 ___sks6___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
